4afd3412a0b2101d98c84f2531920ab1ba99b8f2
Achilles Kaloeridis <achilleas90gr@gmail.com>
2019-06-16 15:04:53 +0300

    First actual test

--- src/tests/AppFooter.test.js
+++ src/tests/AppFooter.test.js
@@ -3,9 +3,14 @@ import { shallow } from 'enzyme';
 import AppFooter from '../components/AppFooter';
 
 describe('AppFooter Component', () => {
-
   it("Todo indicator starts with 0 todos", () => {
-    const wrapper = shallow(<AppFooter />);
+    const filters = {
+      "all": true,
+      "active": false,
+      "completed": false
+    }
+
+    const wrapper = shallow(<AppFooter todos={[]} onComplete={() => { }} filters={filters} />);
     const text = wrapper.find(".app-footer-counter").text();
 
     expect(text).toEqual("Tasks: 0");
